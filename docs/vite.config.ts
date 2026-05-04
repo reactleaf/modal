0502b434fd325f946e81e0f4660565c9987731ed
YG <fourwingsy@gmail.com>
2026-05-04 12:25:12 +0900

    feat: bundling with vite

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@reactleaf/modal': path.resolve(dirname, '../app/index.ts'),
+      '@reactleaf/modal': path.resolve(dirname, '../src/index.ts'),
     },
   },
 });
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import dts from "unplugin-dts/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      entryRoot: "src",
+      outDirs: "dist",
+      tsconfigPath: "./tsconfig.json",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es"],
+      fileName: () => "index.js",
+    },
+    rollupOptions: {
+      external: ["classnames", "react", "react-dom", "react/jsx-runtime"],
+    },
+    sourcemap: true,
+  },
+});
